5a928df23766b0669c45bf7aa8ad31d55a0a65bd
Tatsuo Mitsuchi <t.mitsuchi3@gmail.com>
2020-12-13 14:50:19 +0900

    足し算と引き算を追加

--- nanocc.c
+++ nanocc.c
@@ -7,10 +7,42 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  // p はプログラムとなる文字列の先頭を指すポインター
+  char *p = argv[1];
+
+  // intel記法を使う
   printf(".intel_syntax noprefix\n");
+  // main をリンク時に外のファイルから見れるようにする
   printf(".globl main\n");
+  // main ラベル
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  // strtol で ポインターの指す文字列を10進数の数として解釈する
+  // それによって p は数字の終わりまで移動する
+  // %ld は long int
+  // mov rax : rax レジスタに値をセットする
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  // p の指す先が NULL でない限り
+  while (*p) {
+    // もし + なら一文字進めて、次の数字を読んで足し算を出力する
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    // もし - なら一文字進めて、次の数字を読んで引き算を出力する
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+
+  // rax レジスタの値を返す
   printf("  ret\n");
   return 0;
 }
